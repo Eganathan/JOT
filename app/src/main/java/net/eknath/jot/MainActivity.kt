6ba492a2cbc22a77d59f3fd6b72bb01421e0cddd
eganathan.r <eganathan.r@zohocorp.com>
2023-10-18 14:07:41 +0530

    chore: added list screen wip

--- app/src/main/java/net/eknath/jot/MainActivity.kt
+++ app/src/main/java/net/eknath/jot/MainActivity.kt
@@ -1,66 +1,91 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
+@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 
 package net.eknath.jot
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import net.eknath.jot.ui.componenets.NoteDisplayCard
 import net.eknath.jot.ui.theme.JOTTheme
 
+@OptIn(ExperimentalFoundationApi::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JOTTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Scaffold {
-                        LazyColumn(
-                            modifier = Modifier
-                                .padding(it)
-                                .fillMaxWidth()
-                        ) {
-                            item {
-                                Card {
-                                    Row(horizontalArrangement = Arrangement.SpaceBetween) {
-                                        Column(verticalArrangement = Arrangement.SpaceBetween) {
-                                            Text(text = "Income: Rs. 5000/-")
-                                            Text(text = "Expense: Rs. 9999/-")
-                                        }
-                                        Column(verticalArrangement = Arrangement.SpaceEvenly) {
-                                            Text(text = "Due Today Task: 0")
-                                            Text(text = "Over Due Task: 0")
-                                            Text(text = "Logs: 0")
-                                            Text(text = "Events: 0")
-                                        }
-                                    }
-                                }
-                            }
-
-                            item {
-
-                            }
-                        }
-                    }
 
                 }
             }
         }
     }
 }
+
+@Composable
+fun HomeScreen() {
+    Scaffold(bottomBar = {
+        BottomAppBar {}
+    }, floatingActionButton = {
+        FloatingActionButton(
+            onClick = {}, shape = RoundedCornerShape(10.dp)
+        ) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "")
+        }
+    }, floatingActionButtonPosition = FabPosition.End
+    ) {
+        LazyVerticalStaggeredGrid(modifier = Modifier.padding(it),
+            columns = StaggeredGridCells.Fixed(2),
+            contentPadding = PaddingValues(5.dp),
+            content = {
+                item {
+                    NoteDisplayCard(title = "SOme Title Was here ${it}",
+                        description = "Descripton ${it} of the title required! Something, else and something else plus plus and plus lets seeeejdnf sdojn nnidjije jijwe9j dwewr jasdqwe9 odkdqwer",
+                        onClick = {})
+                }
+                items(25) {
+                    NoteDisplayCard(title = "SOme Title Was here ${it}",
+                        description = "Descripton ${it} of the title required!",
+                        onClick = {})
+                }
+            })
+    }
+}
+
+
--- app/src/main/java/net/eknath/jot/ui/componenets/Cards.kt
+++ app/src/main/java/net/eknath/jot/ui/componenets/Cards.kt
@@ -1,1 +1,35 @@
 package net.eknath.jot.ui.componenets
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NoteDisplayCard(
+    modifier: Modifier = Modifier,
+    title: String,
+    description: String,
+    color: Color = MaterialTheme.colorScheme.primary,
+    onClick: () -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .padding(2.5.dp)
+            .wrapContentSize(),
+        shape = RoundedCornerShape(10.dp)
+    ) {
+        Column(modifier = Modifier.padding(10.dp)) {
+            Text(text = title)
+            Text(text = description)
+        }
+
+    }
+}
