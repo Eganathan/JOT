4b1f6f1c5bf1de28a467a51c94a6cb63a0f354aa
eganathan.r <eganathan.r@zohocorp.com>
2023-11-05 19:27:15 +0530

    chore: input screen wip

--- app/src/main/java/net/eknath/jot/ui/screens/HomeScreen.kt
+++ app/src/main/java/net/eknath/jot/ui/screens/HomeScreen.kt
@@ -6,24 +6,38 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import net.eknath.jot.R
 import net.eknath.jot.ui.componenets.NoteDisplayCard
 import net.eknath.jot.ui.screens.states.EditorState
 
@@ -33,17 +47,31 @@ import net.eknath.jot.ui.screens.states.EditorState
 fun HomeScreen(editorState: EditorState) {
     val showCreation = remember { mutableStateOf(false) }
 
-    Scaffold(
-        floatingActionButton = {
-            FloatingActionButton(onClick = {
+    Scaffold(topBar = {
+        CenterAlignedTopAppBar(modifier = Modifier.shadow(elevation = 10.dp), title = {
+            Text(text = "JOT-Notes and more", fontWeight = FontWeight.Medium)
+        }, navigationIcon = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Default.Menu, contentDescription = "")
+            }
+        }, actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Default.Search, contentDescription = "")
+            }
+        })
+    }, floatingActionButton = {
+        FloatingActionButton(
+            onClick = {
                 editorState.resetTextFieldAndSelection()
                 showCreation.value = true
-            }, shape = RoundedCornerShape(10.dp)) {
-                Icon(imageVector = Icons.Default.Add, contentDescription = "")
-            }
-        }, floatingActionButtonPosition = FabPosition.End
+            }, shape = RoundedCornerShape(10.dp)
+        ) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "")
+        }
+    }, floatingActionButtonPosition = FabPosition.End
     ) {
-        LazyColumn(modifier = Modifier.padding(it),
+        LazyColumn(
+            modifier = Modifier.padding(it),
             contentPadding = PaddingValues(5.dp),
             content = {
                 items(editorState.jotterList.value) {
@@ -64,14 +92,13 @@ fun HomeScreen(editorState: EditorState) {
         enter = fadeIn() + slideInHorizontally(),
         exit = fadeOut() + slideOutHorizontally()
     ) {
-        CreationComponent(visibility = showCreation, editorState = editorState,
-            onBackPressed = {
-                if (editorState.selectedJot.value == null) {
-                    editorState.createJot()
-                } else {
-                    editorState.updateJot()
-                }
-                showCreation.value = false
-            })
+        CreationComponent(visibility = showCreation, editorState = editorState, onBackPressed = {
+            if (editorState.selectedJot.value == null) {
+                editorState.createJot()
+            } else {
+                editorState.updateJot()
+            }
+            showCreation.value = false
+        })
     }
 }
