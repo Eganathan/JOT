10da92ee33d4bf1644ff47c90a08b987bb6b12aa
eganathan.r <eganathan.r@zohocorp.com>
2023-11-10 12:55:36 +0530

    chore: removed hilt di

--- app/src/main/java/net/eknath/jot/JotApplication.kt
+++ app/src/main/java/net/eknath/jot/JotApplication.kt
@@ -1,12 +1,1 @@
 package net.eknath.jot
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-
-@HiltAndroidApp
-class JotApplication() : Application() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
